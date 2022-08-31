eb1cee0b92993d37c75956807d79b0dcb048a13f
Anton Ganhammar <ganhammar@gmail.com>
2022-08-31 21:11:47 +0200

    Fix tests by truncating tables instead of deleting them between tests

--- test/OpenIddict.DynamoDB.Tests/Infrastructure/DynamoDbLocalServerUtils.cs
+++ test/OpenIddict.DynamoDB.Tests/Infrastructure/DynamoDbLocalServerUtils.cs
@@ -1,4 +1,5 @@
 using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.DynamoDBv2.Model;
 using Amazon.Runtime;
 
@@ -34,14 +35,95 @@ internal static class DynamoDbLocalServerUtils
             var tables = Client.ListTablesAsync().GetAwaiter().GetResult();
             tables.TableNames.ForEach(tableName =>
             {
-                try
-                {
-                    Client.DeleteTableAsync(tableName).GetAwaiter().GetResult();
-                } catch (ResourceNotFoundException) { }
+                DeleteTableData(tableName).GetAwaiter().GetResult();
             });
 
             Client.Dispose();
             _disposed = true;
         }
+
+        public async Task DeleteTableData(string tableName)
+        {
+            var (numberOfItems, keys) = await GetTableInformation(tableName);
+            var allItems = new List<Dictionary<string, AttributeValue>>();
+            Dictionary<string, AttributeValue>? exclusiveStartKey = default;
+
+            while (numberOfItems > allItems.Count)
+            {
+                var data = await Client.ScanAsync(new ScanRequest
+                {
+                    TableName = tableName,
+                    AttributesToGet = keys.Select(x => x.AttributeName).ToList(),
+                    ExclusiveStartKey = exclusiveStartKey,
+                });
+                allItems.AddRange(data.Items);
+                exclusiveStartKey = data.LastEvaluatedKey;
+            }
+
+            if (allItems.Any() == false)
+            {
+                return;
+            }
+
+            var writeRequests = allItems
+                .Select(x => new WriteRequest
+                {
+                    DeleteRequest = new DeleteRequest
+                    {
+                        Key = x,
+                    },
+                })
+                .ToList();
+
+            var batches = ToChunks(writeRequests, 25);
+
+            foreach (var batch in batches)
+            {
+                var request = new BatchWriteItemRequest
+                {
+                    RequestItems = new Dictionary<string, List<WriteRequest>>
+                    {
+                        { tableName, batch.ToList() },
+                    },
+                };
+
+                await Client.BatchWriteItemAsync(request);
+            }
+        }
+
+        public async Task<(long, IEnumerable<KeyDefinition>)> GetTableInformation(string tableName)
+        {
+            var response = await Client.DescribeTableAsync(new DescribeTableRequest
+            {
+                TableName = tableName,
+            });
+
+            return (response.Table.ItemCount, response.Table.KeySchema.Select(x => new KeyDefinition
+            {
+                AttributeName = x.AttributeName,
+                AttributeType = response.Table.AttributeDefinitions
+                    .First(y => y.AttributeName == x.AttributeName)
+                    .AttributeType,
+                KeyType = x.KeyType,
+            }));
+        }
+
+        private IEnumerable<IEnumerable<T>> ToChunks<T>(List<T> fullList, int batchSize)
+        {
+            var total = 0;
+
+            while (total < fullList.Count)
+            {
+                yield return fullList.Skip(total).Take(batchSize);
+                total += batchSize;
+            }
+        }
+    }
+
+    public class KeyDefinition
+    {
+        public string? AttributeName { get; set; }
+        public ScalarAttributeType? AttributeType { get; set; }
+        public KeyType? KeyType { get; set; }
     }
 }
